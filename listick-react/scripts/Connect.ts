57d018d3aa383b981f8729478f4330aa796c58a2
Artem Labutin <artemkasan83@gmail.com>
2018-03-24 09:38:38 +0100

    * Inform if state is missed in connect. * Notify if state modified. * Functional tests extended

--- listick-react/scripts/Connect.ts
+++ listick-react/scripts/Connect.ts
@@ -22,30 +22,31 @@ export interface ConnectClassDecorator<TProps>
  * Connect React component with Listick Store. 
  * @param stateGet Callback that extract required state from store.
  */
-export function connect<TProps, TState>(stateGet: (store: any) => TState): ConnectClassDecorator<TProps>
+export function connect<TProps, TState>(stateGet: (store: any) => TState)
+	: ConnectClassDecorator<TProps>
 {
 
 	let subscribedFunction: (sender: any, args: any)=> void;
 
-	return <T extends ConnectComponentClass<TProps>>(target: T): T =>
-	{
+	return <T extends ConnectComponentClass<TProps>>(target: T): T => {
 		// Here we override real rect component with new one.
 		// For this new constructor is defined and prototype copied to new object.
 		let store: Store<any>;
 		const inputArgs: any[] = Reflect.getMetadata("design:paramtypes", target);
-		function constructorOverride(props: TProps, context: any, pThis: any)
-		{
+		function constructorOverride(props: TProps, context: any, pThis: any) {
 			// Collect all services requested by constructor.
 			const requestedServices: any[] = [];
 			store = context.store as Store<any>;
+			if(store == null) {
+				console.warn('You need to wrap listick component with root \
+StoreContainer component.')
+			}
+
 			// if there is no constructor in component input args will be undefined.
-			if(inputArgs !== undefined)
-			{
-				for (let i = 2; i < inputArgs.length; i++)
-				{
+			if(inputArgs !== undefined) {
+				for (let i = 2; i < inputArgs.length; i++) {
 					const service = store.getService(inputArgs[i]);
-					if (service === undefined)
-					{
+					if (service === undefined) {
 						console.error(`Service ${inputArgs[i]} is not registered in store`);
 					}
 
@@ -59,7 +60,9 @@ export function connect<TProps, TState>(stateGet: (store: any) => TState): Conne
 				...requestedServices
 			];
 			target.apply(pThis, targetArgs);
-			pThis.state = stateGet(store.getStoreState());
+			if(store != null) {
+				pThis.state = stateGet(store.getStoreState());
+			}
 		}
 
 		// Have to use this hack to make new class name the same as original.
@@ -73,18 +76,29 @@ export function connect<TProps, TState>(stateGet: (store: any) => TState): Conne
 		connector.prototype = Object.create(target.prototype);
 
 		// We need to listen to state change only when component is mounted.
-		connector.prototype.componentDidMount = function()
-		{
-			subscribedFunction = (sender: any, args: any) =>
-			{
-				this.setState(stateGet(store.getStoreState()));
-			};
-			store.stateChanged.add(subscribedFunction)
+		connector.prototype.componentDidMount = function() {
+			if(store != null) {
+				subscribedFunction = (sender: any, args: any) => {
+					this.setState(stateGet(store.getStoreState()));
+				};
+				store.stateChanged.add(subscribedFunction)
+			}
 		};
-		connector.prototype.componentWillUnmount = function()
-		{
-			store.stateChanged.remove(subscribedFunction);
+
+		connector.prototype.componentWillUnmount = function() {
+			if(store != null) {
+				store.stateChanged.remove(subscribedFunction);
+			}
 		};
+
+		connector.prototype.setState = function() {
+			if(store != null) {
+				console.warn("Attempt to change state from React component, \
+but this component is connected to listick state. \
+State change ignored.");
+			}
+		};
+
 		connector.contextTypes = {
 			store: PropTypes.object.isRequired
 		}
